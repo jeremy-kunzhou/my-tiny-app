4ac5d371779811b0f87610a9187017131a04207b
jeremy <clirochina@gmail.com>
2023-02-13 15:10:28 +1100

    update
    1 fixed bug on copy json string
    2 update file upload

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
-# My Tiny Tools
+# simple-chat
+A simple chat application in nodejs and mongodb
 
-## Description
-works in local network environment
-
-provides:
-
-1 file sharing via upload and online view
-
-2 small content via qrcode generation and sharing
-
-3 live chat and sharing text content
-
-## How to work:
-
-1 `npm install` to init dependent library
-
-2 `node ./server.js` to start server
-
-3 visit `http://localhost:3000` to see the dashboard
+Read
+https://medium.com/@amkurian/simple-chat-application-in-node-js-using-express-mongoose-and-socket-io-ee62d94f5804
--- server.js
+++ server.js
@@ -1,35 +1,68 @@
-var express = require('express');
-const fileUpload = require('express-fileupload');
-var bodyParser = require('body-parser');
-var path = require('path');
+var express = require("express");
+const fileUpload = require("express-fileupload");
+var bodyParser = require("body-parser");
+var path = require("path");
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+const multer = require("multer");
 
-var serveIndex = require('serve-index')
+var serveIndex = require("serve-index");
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-function MessageStorage () {
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, "./upload");
+    },
+    filename(req, file, callback) {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      callback(
+        null,
+        `${file.fieldname}_${Date.now()}_${file.originalname}.${ext}`
+      );
+    },
+  }),
+  limits: { fieldSize: 25 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error("Invalid mime type!");
+    cb(error, isValid);
+  },
+});
+
+function MessageStorage() {
   const allMessages = [];
   return {
     find: function (conditions, callback) {
-      const filter = allMessages.filter(element => {
-        return Object.keys(conditions).map(key => {
-          return element.hasOwnProperty(key) && element.key === conditions[key]
-        }).filter(e => e).length === Object.keys(conditions).length
-      })
-      callback('', filter)
+      const filter = allMessages.filter((element) => {
+        return (
+          Object.keys(conditions)
+            .map((key) => {
+              return (
+                element.hasOwnProperty(key) && element.key === conditions[key]
+              );
+            })
+            .filter((e) => e).length === Object.keys(conditions).length
+        );
+      });
+      callback("", filter);
     },
     save: function (name, message) {
       allMessages.push({
-        name, message
-      })
-    }
-
-  }
+        name,
+        message,
+      });
+    },
+  };
 }
 
 var Message = MessageStorage();
@@ -37,75 +70,76 @@ var Message = MessageStorage();
 // Serve URLs like /ftp/thing as public/ftp/thing
 // The express.static serves the file contents
 // The serveIndex is this module serving the directory
-app.use('/files', express.static(path.join(__dirname, 'upload')), serveIndex(path.join(__dirname, 'upload'), {'icons': true}))
+app.use(
+  "/files",
+  express.static(path.join(__dirname, "upload")),
+  serveIndex(path.join(__dirname, "upload"), { icons: true })
+);
 
-app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
+app.get("/messages", (req, res) => {
+  Message.find({}, (err, messages) => {
     res.send(messages);
-  })
-})
+  });
+});
 
-
-app.get('/messages/:user', (req, res) => {
-  var user = req.params.user
-  Message.find({name: user},(err, messages)=> {
+app.get("/messages/:user", (req, res) => {
+  var user = req.params.user;
+  Message.find({ name: user }, (err, messages) => {
     res.send(messages);
-  })
-})
+  });
+});
 
-
-app.post('/messages', async (req, res) => {
-  try{
-
-    Message.save(req.body.name, req.body.message)
-      console.log('saved');
+app.post("/messages", async (req, res) => {
+  try {
+    Message.save(req.body.name, req.body.message);
+    console.log("saved");
 
     // var censored = await Message.findOne({message:'badword'});
     //   if(censored)
     //     await Message.remove({_id: censored.id})
     //   else
-        io.emit('message', req.body);
-      res.sendStatus(200);
-  }
-  catch (error){
+    io.emit("message", req.body);
+    res.sendStatus(200);
+  } catch (error) {
     res.sendStatus(500);
-    return console.log('error',error);
+    return console.log("error", error);
+  } finally {
+    console.log("Message Posted");
   }
-  finally{
-    console.log('Message Posted')
-  }
+});
 
-})
-
-app.post('/upload', function(req, res) {
+app.post("/upload", fileUpload(), function (req, res) {
   let sampleFile;
   let uploadPath;
 
+  console.log("upload call");
   if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
+    return res.status(400).send("No files were uploaded.");
   }
 
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   sampleFile = req.files.sampleFile;
-  uploadPath = __dirname + '/upload/' + sampleFile.name;
+  uploadPath = __dirname + "/upload/" + sampleFile.name;
 
   // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(uploadPath, function(err) {
-    if (err)
-      return res.status(500).send(err);
+  sampleFile.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
 
-    res.send('File uploaded!');
+    res.send("File uploaded!");
   });
 });
 
+app.post("/uploadPhoto", upload.array("photo", 3), function (req, res) {
+  if (!req.files) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  res.send("File uploaded!");
+});
 
+io.on("connection", () => {
+  console.log("a user is connected");
+});
 
-io.on('connection', () =>{
-  console.log('a user is connected')
-})
-
-
-
-var server = http.listen(3000, '192.168.2.107', () => {
-  console.log('server is running on port', server.address().port);
+var server = http.listen(3000, "192.168.2.107", () => {
+  console.log("server is running on port", server.address().port);
 });
